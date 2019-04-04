683886acf1ad9f3cb414846cae581dcb1f57a579
Piotrek <piotrek.chrzan@gmail.com>
2019-04-04 17:47:35 +0200

    Created async task for map generation -> Fixed bug with fast clicking and app freezing.

---
+++ app/src/main/java/com/example/nosmogrunner/AirlyGetter.java
@@ -0,0 +1,13 @@
+package com.example.nosmogrunner;
+
+import android.os.AsyncTask;
+
+import java.util.ArrayList;
+
+public class AirlyGetter  {
+
+
+
+
+
+}
--- app/src/main/java/com/example/nosmogrunner/MapActivity.java
+++ app/src/main/java/com/example/nosmogrunner/MapActivity.java
@@ -1,6 +1,7 @@
 package com.example.nosmogrunner;
 
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -44,67 +45,109 @@ public class MapActivity extends MainMenuActivity implements OnMapReadyCallback 
         LatLng warsaw = new LatLng(52.2319237, 21.0067265);
         LatLng northeast = new LatLng(52.4836,21.2837);
         LatLng southwest = new LatLng(51.9871,20.7452);
-
+        LatLngBounds warsawBounds = new LatLngBounds(southwest,northeast);
         googleMap.addMarker(new MarkerOptions().position(warsaw)
                 .title("Marker in Warsaw"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(warsaw));
 
-
+        DrawPolyline myDrawing = new DrawPolyline();
         PolylineOptions borderPoly = new PolylineOptions();
+        MyTaskParams parameters = new MyTaskParams(googleMap,warsawBounds,borderPoly);
+        myDrawing.execute(parameters);
 
 
-        ArrayList<LatLng> myBorderArray2 = new ArrayList<LatLng>();
-        myBorderArray2 = getJSON();
-        borderPoly.addAll(myBorderArray2);
-        borderPoly.width(5f);
-        borderPoly.color(Color.RED);
-        googleMap.addPolyline(borderPoly);
 
-        LatLngBounds warsawBounds = new LatLngBounds(southwest,northeast);
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngBounds(warsawBounds,0));
+
+
     }
-public ArrayList<LatLng> getJSON()
-{
-    ArrayList<LatLng> myBorderArray = new ArrayList<LatLng>();
-    String json;
-    try
+
+    private static class MyTaskParams {
+        GoogleMap googleMap;
+        LatLngBounds warsawBounds;
+        PolylineOptions borderPoly;
+
+        MyTaskParams(GoogleMap googleMap, LatLngBounds warsawBounds, PolylineOptions borderPoly) {
+            this.googleMap = googleMap;
+            this.warsawBounds = warsawBounds;
+            this.borderPoly = borderPoly;
+        }
+    }
+    private class DrawPolyline extends AsyncTask<MyTaskParams, Integer, MyTaskParams>
     {
-        InputStream is = getAssets().open("boundaries.json");
-        int size = is.available();
-        byte[] buffer = new byte[size];
-        is.read(buffer);
-        is.close();
-
-        json = new String(buffer, "UTF-8");
-        JSONArray jsonArray = new JSONArray(json);
+        @Override
+        protected MyTaskParams doInBackground(MyTaskParams... parameters){
 
 
-        double[] Latitudes = new double[935];
-        double[] Longitudes = new double[935];
-        for (int i = 0; i<jsonArray.length();i++)
-        {
-            JSONArray value = jsonArray.getJSONArray(i);
-            Latitudes[i] = value.getDouble(0);
-            Longitudes[i] = value.getDouble(1);
-            myBorderArray.add(i,new LatLng( Longitudes[i],Latitudes[i]));
+            ArrayList<LatLng> myArray = new ArrayList();
+            ArrayList<LatLng> myBorderArray2= getJSON();
+            PolylineOptions borderPoly = parameters[0].borderPoly;
+            borderPoly.addAll(myBorderArray2);
+            borderPoly.width(5f);
+            borderPoly.color(Color.RED);
 
+
+
+
+
+
+            return parameters[0];
+        }
+
+        @Override
+        protected void onPostExecute(MyTaskParams parameters) {
+            GoogleMap googleMap = parameters.googleMap;
+            PolylineOptions borderPoly = parameters.borderPoly;
+            LatLngBounds warsawBounds = parameters.warsawBounds;
+            googleMap.addPolyline(borderPoly);
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngBounds(warsawBounds,0));
         }
 
 
-      //  JSONObject lastObj = obj.getJSONObject("0");
+
+        public ArrayList<LatLng> getJSON()
+        {
+            ArrayList<LatLng> myBorderArray = new ArrayList<LatLng>();
+            String json;
+            try
+            {
+                InputStream is = getAssets().open("boundaries.json");
+                int size = is.available();
+                byte[] buffer = new byte[size];
+                is.read(buffer);
+                is.close();
+
+                json = new String(buffer, "UTF-8");
+                JSONArray jsonArray = new JSONArray(json);
 
 
+                double[] Latitudes = new double[935];
+                double[] Longitudes = new double[935];
+                for (int i = 0; i<jsonArray.length();i++)
+                {
+                    JSONArray value = jsonArray.getJSONArray(i);
+                    Latitudes[i] = value.getDouble(0);
+                    Longitudes[i] = value.getDouble(1);
+                    myBorderArray.add(i,new LatLng( Longitudes[i],Latitudes[i]));
+
+                }
+
+
+                //  JSONObject lastObj = obj.getJSONObject("0");
+
+
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+            catch (JSONException j)
+            {
+                j.printStackTrace();
+            }
+            return myBorderArray;
+        }
+
     }
-    catch(IOException e)
-    {
-        e.printStackTrace();
-    }
-    catch (JSONException j)
-    {
-        j.printStackTrace();
-    }
-    return myBorderArray;
-}
 
 
 
